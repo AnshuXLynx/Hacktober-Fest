942aa1e922afe525b9371e886415be621f57c9ba
AnshuXLynx <anshumandash2006pro@gmail.com>
2024-10-05 12:55:56 -0700

    11C.c

--- C/Easy/11.c
+++ C/Easy/11.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[5] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(int);
     for (int i = 0; i <= size; i++) {
-        printf("%d ", arr[i]);
+        printf("%d\n ", arr[i]);
     }
-    printf("\n");
     return 0;
 }
