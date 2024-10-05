7342c08e8b1e86483fa123f5518a0bcc7cad0984
AnshuXLynx <anshumandash2006pro@gmail.com>
2024-10-05 13:38:55 -0700

    35C.c

--- C/Easy/35.c
+++ C/Easy/35.c
@@ -2,22 +2,22 @@
 
 int main() {
     
-    int array1[] = {1, 2, 3}; 
-    int array2[] = {4, 5, 6}; 
+    int array1[2] = {1, 2, 3}; 
+    int array2[2] = {4, 5, 6}; 
     int concatenated[6];     
 
     
-    for (int i = 0; i < __; i++) {
-       _______ = array1[i];
+    for (int i = 0; i < 3; i++) {
+       concatenated[3] = array1[i];
     }
 
    
     for (int i = 0; i < 3; i++) {
-        concatenated[____] = array2[i];
+        concatenated[3] = array2[i];
     }
 
  
-    for (int i = 0; i < __; i++) {
+    for (int i = 0; i < 7 ; i++) {
         printf("%d ", concatenated[i]);
     }
     printf("\n");
